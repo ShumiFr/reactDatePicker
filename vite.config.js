b0f7d6f3d994f4dfce4ea10960b9c7899403ae70
Gagnaire Pauline <gagnaire.pauline@gmail.com>
2025-02-04 14:36:31 +0100

    Petit changement apporter pour l'import du package

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.js"),
+      name: "ReactDatePicker",
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      // Assurez-vous d'externaliser les dépendances que vous ne voulez pas inclure dans votre bundle
+      external: [
+        "react",
+        "react-dom",
+        "moment",
+        "@fortawesome/react-fontawesome",
+        "@fortawesome/free-solid-svg-icons",
+      ],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+          moment: "moment",
+          "@fortawesome/react-fontawesome": "FontAwesome",
+          "@fortawesome/free-solid-svg-icons": "FontAwesomeIcons",
+        },
+      },
+    },
+  },
+});
