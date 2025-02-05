b456671b3c72c915eaa9a114c5853562db9c563a
Gagnaire Pauline <gagnaire.pauline@gmail.com>
2025-02-05 17:14:55 +0100

    Petit debug sur le vite config

--- src/lib/DatePicker.jsx
+++ src/lib/DatePicker.jsx
@@ -1,4 +1,6 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable react-hooks/rules-of-hooks */
+
 import { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, "src/lib/index.js"),
-      name: "ReactDatePicker",
-      fileName: (format) => `index.${format}.js`,
-    },
-    rollupOptions: {
-      external: [
-        "react",
-        "react-dom",
-        "moment",
-        "@fortawesome/react-fontawesome",
-        "@fortawesome/free-solid-svg-icons",
-      ],
-      output: {
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
-          moment: "moment",
-          "@fortawesome/react-fontawesome": "FontAwesome",
-          "@fortawesome/free-solid-svg-icons": "FontAwesomeIcons",
-        },
-      },
-    },
-  },
 });
